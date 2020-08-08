9c296769b74df7f3d0e0d1a4773953a9f0dc60e4
Doug Gilliland <doug@douglasgilliland.com>
2020-08-08 13:48:25 -0400

    Command interface

--- 6502/OSI/OSISDOS/hello.c
+++ 6502/OSI/OSISDOS/hello.c
@@ -1,6 +1,11 @@
 /* hello.c example	*/
 /* conio.h docs - Not sure how well it matches	*/
 /* 	https://digitalmars.com/rtl/conio.html		*/
+/* Commands are																					*/
+/* mnt - mount the SD card, load/parse the boot record 											*/
+/* dir - fetch the directory from the SD card and display to the screen, starts in root folder	*/
+/* cd - change directory																		*/
+/* ld - load a file from the SD card															*/
 
 #include <conio.h>	/* Console I/O	*/
 #include <stdlib.h>
@@ -14,11 +19,28 @@ int main (void)
 	{
 		if (rowCount == 0)
 			clrscr();
-		cprintf("Press x to exit <RETURN>.");
+		cprintf("\n\rCommand (M/D/C/L/R/B/W/?/X) > ");
 		myChar = cgetc ();
 		if (myChar == 'X')
 			loop = 0;
-		cprintf("%c\n\r",myChar);
+		else if (myChar == 'M')
+			cprintf("Mount ");
+		else if (myChar == 'D')
+			cprintf("Dir ");
+		else if (myChar == 'C')
+			cprintf("Chdir ");
+		else if (myChar == 'L')
+			cprintf("Load ");
+		else if (myChar == 'R')
+			cprintf("Run ");
+		else if (myChar == 'S')
+			cprintf("Save ");
+		else if (myChar == 'B')
+			cprintf("Basic ");
+		else if (myChar == 'W')
+			cprintf("Warm Start ");
+		else
+			cprintf("\n\rCommands: Mount/Dir/Chdir/Load/Run/Save/Basic/Warm/eXit");
 		if (rowCount++ == 14)
 			rowCount = 0;
 	}
