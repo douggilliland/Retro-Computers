f28e8b2fa75b57ca78cfb3d6b551dd5180b15b39
Doug Gilliland <doug@douglasgilliland.com>
2020-09-12 08:18:44 -0400

    Linked TG68 AMR Software to Linux-68k Repo

--- 68000/TG68_AMR/TG68_AMR_Software/Apps/Screenmodes/minisoc_hardware.c
+++
@@ -1,3 +0,0 @@
-
-#include "minisoc_hardware.h"
-
--- 68000/TG68_AMR/TG68_AMR_Software/Firmware_Common/interrupts.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTS_H
-#define INTS_H
-
-void SetIntHandler(short interrupt, void(*handler)());
-extern void EnableInterrupts();
-extern void DisableInterrupts();
-
-#endif
