72b7ee2adf2623f5448f247f9c1d56fb4469ece7
Doug Gilliland <doug@douglasgilliland.com>
2021-04-09 04:58:05 -0400

    PDP-8 files

--- PDP-8/PDP8_Book(Almy)/VHDL/panel_phoney.vhd
+++ PDP-8/PDP8_Book(Almy)/VHDL/panel_phoney.vhd
@@ -27,10 +27,6 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Panel_Phoney is
 	Port (
