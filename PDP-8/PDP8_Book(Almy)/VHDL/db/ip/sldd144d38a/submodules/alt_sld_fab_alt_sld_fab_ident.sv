9d5ee536390c8447c1a06a7927468383c827d6b0
Doug Gilliland <doug@douglasgilliland.com>
2021-05-15 16:19:19 -0400

    Removed SignalTap from build

--- PDP-8/PDP8_Book(Almy)/VHDL/db/ip/sldd144d38a/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ PDP-8/PDP8_Book(Almy)/VHDL/db/ip/sldd144d38a/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : f3bc5d8af699e04c73db
+//   DESIGN_HASH   : 08512a08bd626154ebe3
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf3bc5d8af699e04c73db };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h08512a08bd626154ebe3 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
