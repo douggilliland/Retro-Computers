6c134513125f93fd7b5a1ce4c454620b2ed47760
Doug Gilliland <doug@douglasgilliland.com>
2024-09-12 18:06:21 -0400

    ACIA TEST WORKS

--- 6502/LB-6502/AciaTest/AciaTest.bat
+++ 6502/LB-6502/AciaTest/AciaTest.bat
@@ -1,4 +1,3 @@
 ..\ca65 AciaTest.s -o AciaTest.o -l
-REM..\ld65 -C AciaTest.cfg AciaTest.o -o AciaTest.bin
-..\ld65 -t none AciaTest.o -o AciaTest.bin
+..\ld65 -C AciaTest.cfg AciaTest.o -o AciaTest.bin
 ..\srec_cat.exe AciaTest.bin -binary -o AciaTest.hex -Intel
--- 6502/LB-6502/AciaTest/AciaTest.s
+++ 6502/LB-6502/AciaTest/AciaTest.s
@@ -9,9 +9,8 @@ ACIAControl := ACIA+0
 ACIAStatus := ACIA+0
 ACIAData := ACIA+1
 
-.segment "IOHANDLER"
-.org $C000
 .segment "CODE"
+.org $C000
 
 Reset:
 	LDX     #$7F
@@ -21,15 +20,21 @@ Reset:
 	STA	ACIAControl
 	LDA 	#$15		; Set ACIA baud rate, word size and Rx interrupt (to control RTS)
 	STA	ACIAControl
+	NOP
+	LDA	ACIAData
 LOOP1:
 	LDA	ACIAStatus
-	AND	#1
-	CMP	#1
-	BEQ	LOOP1
+	AND #1
+	CMP #1
+	BNE LOOP1
+	NOP
 	LDA	ACIAData
+	NOP
+	STA ACIAData
+	NOP
     JMP	LOOP1
 
-;.segment "VECTS"
+.segment "VECTS"
 .org $FFFA
 	.word	Reset		; NMI 
 	.word	Reset		; RESET 
